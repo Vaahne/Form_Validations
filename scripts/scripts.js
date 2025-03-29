20cd5b47faff0de978575e4a262919462a89d0b7
Spring -Git <vaahne@yahoo.com>
2025-03-28 22:54:12 -0600

    Some validations

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -109,3 +109,44 @@ function validatePasswordCheck(ps1,ps2){
 }
 // console.log(password.value);
 
+
+// Login Form
+let loginForm = document.getElementById("login");
+
+loginForm.addEventListener('submit',loginValidation);
+
+function loginValidation(e){
+    e.preventDefault();
+    err.style.display = "none";
+    
+    let userName = loginForm.querySelector("#userName");
+    let password = loginForm.querySelector("#password");
+
+    let users = JSON.parse(localStorage.getItem("users")) || [];
+
+    if(users.length == 0){
+        alert("No Users yet. Please register first!!");
+        return ;
+    }
+    else{
+        let found = false;
+        
+        for(user of users){            
+            if(user.userName === userName.value && user.password === password.value){
+                alert(`Welcome ${userName.value}`);
+                loginForm.reset();
+                found = true;
+                return;
+            }
+        }
+        if(!found){
+            let h1 = document.createElement ("p");
+            err.style.display = "block";
+            h1.textContent = "User doesnot exist Please register !!!"; 
+            err.appendChild(h1);     
+        }
+            
+    }
+
+
+}
